5278a5d0ad19a01a60abe6055eb8f8588c79c4cf
Dulran Hemjitha <dhemjitha@gmail.com>
2025-03-19 11:47:55 +0530

    fixed: type errors

--- src/api/middleware/authorization-middleware.ts
+++ src/api/middleware/authorization-middleware.ts
@@ -1,8 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import ForbiddenError from '../../domain/errors/forbidden-error';
-import { clerkClient } from "@clerk/express";
 
 export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
+    //@ts-ignore
     if (!(req?.auth?.sessionClaims?.role !== "admin")) {
         throw new ForbiddenError("Forbidden");
     }
