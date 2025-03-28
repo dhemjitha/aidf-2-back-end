2ae40e8d4e15b74b3c9d8e474b273d5cb54e1dc8
Dulran Hemjitha <dhemjitha@gmail.com>
2025-03-28 15:05:41 +0530

    fixed: update port

--- src/index.ts
+++ src/index.ts
@@ -22,5 +22,5 @@ app.use("/api/bookings", bookingRouter);
 
 app.use(globalErrorHandlingMiddleware);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`));
